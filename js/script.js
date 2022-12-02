a01f2ed6a0327827d8e30c453141b010c6662a05
EvaXausa <xausa.eva@gmail.com>
2022-12-02 17:46:27 +0100

    promts

--- js/script.js
+++ js/script.js
@@ -3,6 +3,18 @@ console.log("JS OK");
 1 - Chiedere il nome all'utente
 2 - Salvarlo in una variabile
 3 - Prendo l'elemento dalla pagina
+4 - Chiedere il cognome all'utente 
+5 - Salvarlo in una variabile 
+6 - Prendo l'elemento dalla pagina 
+7 - Chiedere il colore preferito 
+8 - Salvarlo in una variabile 
 */
+
 const firstName = prompt("Come ti chiami?", "Eva");
 console.log(firstName);
+
+const lastName = prompt("Qual'e' il tuo cognome?", "Xausa");
+console.log(lastName);
+
+const favouritecolor = prompt("Qual'e' il tuo colore preferito?", "giallo");
+console.log(favouritecolor);
