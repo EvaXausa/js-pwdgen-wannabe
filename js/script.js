a7b05d9515fa959f84d0b8fe7c49e5a28a568dc9
EvaXausa <xausa.eva@gmail.com>
2022-12-02 16:50:17 +0100

    name

---
+++ js/script.js
@@ -0,0 +1,8 @@
+console.log("JS OK");
+/*
+1 - Chiedere il nome all'utente
+2 - Salvarlo in una variabile
+3 - Prendo l'elemento dalla pagina
+*/
+const firstName = prompt("Come ti chiami?", "Eva");
+console.log(firstName);
